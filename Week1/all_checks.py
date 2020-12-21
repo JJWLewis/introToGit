a5da4a000430a4181e470285903f25c419238550
Jon-Jacques Lewis <13749976+JJWLewis@users.noreply.github.com>
2020-12-21 12:47:03 +0000

    yapf formatting

--- Week1/all_checks.py
+++ Week1/all_checks.py
@@ -6,9 +6,11 @@ import sys
 import socket
 import psutil
 
+
 def check_reboot():
     """Returns True is the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
+
 
 def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
@@ -21,9 +23,11 @@ def check_disk_full(disk, min_gb, min_percent):
         return True
     return False
 
+
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 
 def check_no_network():
     """Returns True if it fails to resolve Google's URL, False otherwise."""
@@ -33,24 +37,24 @@ def check_no_network():
     except:
         return True
 
+
 def check_cpu_constrained():
     """Returns True if the cpu is having too much usage, False otherwise."""
     return psutil.cpu_percent(1) > 75
 
+
 def main():
-    checks=[
-        (check_reboot, "Pending Reboot"),
-        (check_root_full, "Root partition full"),
-        (check_no_network, "No working network"),
-        (check_cpu_constrained, "CPU load too high")
-    ]
+    checks = [(check_reboot, "Pending Reboot"),
+              (check_root_full, "Root partition full"),
+              (check_no_network, "No working network"),
+              (check_cpu_constrained, "CPU load too high")]
     everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
-            everything_ok= False
-    
+            everything_ok = False
     if not everything_ok:
         sys.exit(1)
+
 
 main()
